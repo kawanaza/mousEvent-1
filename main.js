d4a06047d6dd927b22b7b83897e6a5f2862736c2
kawan <89071329+kawanaza@users.noreply.github.com>
2022-01-19 09:30:18 +0100

    done

---
+++ main.js
@@ -0,0 +1,176 @@
+function clickMe1() {
+  document.getElementById("clock1").style.backgroundColor = "red";
+  document.getElementById("ulSumme").innerHTML =
+    document.getElementById("ulSumme").innerHTML +
+    "<li>" +
+    "Zivo 299.0 €" +
+    "</li>";
+  document.getElementById("total11").innerHTML =
+    Number(document.getElementById("total11").innerHTML) +
+    Number(document.getElementById("price1").innerHTML);
+}
+
+function outsidePic1() {
+  document.getElementById("img1w").src = "./zinvo-1.jpg";
+
+  document.getElementById("img1w").style.width = "20rem";
+  document.getElementById("img1w").style.height = "20rem";
+}
+function insidePic1() {
+  document.getElementById("img1w").src = "./zinvo-1.jpg";
+  document.getElementById("img1w").style.width = "25rem";
+  document.getElementById("img1w").style.height = "25rem";
+}
+////////////////// (2)////////////////////////////////
+function clickMe2() {
+  document.getElementById("clock2").innerHTML = "buy Now";
+  document.getElementById("ulSumme").innerHTML =
+    document.getElementById("ulSumme").innerHTML +
+    "<li>" +
+    "Zivo 350.0 €" +
+    "</li>";
+}
+
+function outsidePic2() {
+  document.getElementById("img2w").src = "/zinvo-2.jpg";
+
+  document.getElementById("img2w").style.width = "20rem";
+  document.getElementById("img2w").style.height = "20rem";
+}
+function insidePic2() {
+  document.getElementById("img2w").src = "./zinvo-2.jpg";
+  document.getElementById("img2w").style.width = "25rem";
+  document.getElementById("img2w").style.height = "25rem";
+}
+////////////////// (3)////////////////////////////////
+function clickMe3() {
+  document.getElementById("clock3").innerHTML = "buy Now";
+}
+
+function outsidePic3() {
+  document.getElementById("img3w").src = "/zinvo-3.jpg";
+
+  document.getElementById("img3w").style.width = "20rem";
+  document.getElementById("img3w").style.height = "20rem";
+}
+function insidePic3() {
+  document.getElementById("img3w").src = "/zinvo-3.jpg";
+  document.getElementById("img3w").style.width = "25rem";
+  document.getElementById("img3w").style.height = "25rem";
+}
+////////////////// (4)////////////////////////////////
+function clickMe4() {
+  document.getElementById("clock3").innerHTML = "buy Now";
+}
+
+function outsidePic4() {
+  document.getElementById("img4w").src = "./zinvo-4.jpg";
+
+  document.getElementById("img4w").style.width = "20rem";
+  document.getElementById("img4w").style.height = "20rem";
+}
+function insidePic4() {
+  document.getElementById("img4w").src = "/zinvo-4.jpg";
+  document.getElementById("img4w").style.width = "25rem";
+  document.getElementById("img4w").style.height = "25rem";
+}
+////////////////// (5)////////////////////////////////
+function clickMe5() {
+  document.getElementById("clock5").innerHTML = "buy Now";
+}
+
+function outsidePic5() {
+  document.getElementById("img5w").src = "/zinvo-5.jpg";
+
+  document.getElementById("img5w").style.width = "20rem";
+  document.getElementById("img5w").style.height = "20rem";
+}
+function insidePic5() {
+  document.getElementById("img5w").src = "/zinvo-5.jpg";
+  document.getElementById("img5w").style.width = "25rem";
+  document.getElementById("img5w").style.height = "25rem";
+}
+
+////////////////// (6)////////////////////////////////
+function clickMe6() {
+  document.getElementById("clock6").innerHTML = "buy Now";
+}
+
+function outsidePic6() {
+  document.getElementById("img6w").src = "/zinvo-6.jpg";
+
+  document.getElementById("img6w").style.width = "20rem";
+  document.getElementById("img6w").style.height = "20rem";
+}
+function insidePic6() {
+  document.getElementById("img6w").src = "/zinvo-6.jpg";
+  document.getElementById("img6w").style.width = "25rem";
+  document.getElementById("img6w").style.height = "25rem";
+}
+////////////////// (7)////////////////////////////////
+function clickMe7() {
+  document.getElementById("clock7").innerHTML = "buy Now";
+}
+
+function outsidePic7() {
+  document.getElementById("img7w").src = "/zinvo-7.jpg";
+
+  document.getElementById("img7w").style.width = "20rem";
+  document.getElementById("img7w").style.height = "20rem";
+}
+function insidePic7() {
+  document.getElementById("img7w").src = "//zinvo-7.jpg";
+  document.getElementById("img7w").style.width = "25rem";
+  document.getElementById("img7w").style.height = "25rem";
+}
+
+////////////////// (8)////////////////////////////////
+function clickMe8() {
+  document.getElementById("clock8").innerHTML = "buy Now";
+}
+
+function outsidePic8() {
+  document.getElementById("img8w").src = "/zinvo-8.jpg";
+
+  document.getElementById("img8w").style.width = "20rem";
+  document.getElementById("img8w").style.height = "20rem";
+}
+function insidePic8() {
+  document.getElementById("img8w").src = "/zinvo-8.jpg";
+  document.getElementById("img8w").style.width = "25rem";
+  document.getElementById("img8w").style.height = "25rem";
+}
+
+////////////////// (9)////////////////////////////////
+function clickMe9() {
+  document.getElementById("clock9").innerHTML = "buy Now";
+}
+
+function outsidePic9() {
+  document.getElementById("img9w").src = "/zinvo-9.jpg";
+
+  document.getElementById("img9w").style.width = "20rem";
+  document.getElementById("img9w").style.height = "20rem";
+}
+function insidePic9() {
+  document.getElementById("img9w").src = "/zinvo-9.jpg";
+  document.getElementById("img9w").style.width = "25rem";
+  document.getElementById("img9w").style.height = "25rem";
+}
+
+////////////////// (10)////////////////////////////////
+function clickMe10() {
+  document.getElementById("clock10").innerHTML = "buy Now";
+}
+
+function outsidePic10() {
+  document.getElementById("img3w").src = "//zinvo-10.jpg";
+
+  document.getElementById("img10w").style.width = "20rem";
+  document.getElementById("img10w").style.height = "20rem";
+}
+function insidePic10() {
+  document.getElementById("img10w").src = "/zinvo-10.jpg";
+  document.getElementById("img10w").style.width = "25rem";
+  document.getElementById("img10w").style.height = "25rem";
+}
